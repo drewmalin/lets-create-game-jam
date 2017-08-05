7cf60f4084eddda9bd0adadf39a4c5f2b417b39e
Drew Malin <drew.malin@gmail.com>
2017-08-05 10:30:39 -0700

    Updates to PlayerMovement to rotate the character to the location of the mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-  public float playerSpeed = 5.0f;
+    public float playerSpeed = 5.0f;
 
-  private Rigidbody playerRigidbody;
-  private Vector3 movement;
+    private Rigidbody playerRigidbody;
+    private Vector3 movement;
+    private int floorMask;
 
-	// Use this for initialization
-	void Start () {
-    playerRigidbody = GetComponent<Rigidbody> ();
-	}
-	
-	void FixedUpdate () {
-    float h = Input.GetAxisRaw ("Horizontal");
-    float v = Input.GetAxisRaw ("Vertical");
+    void Awake() {
+        this.floorMask = LayerMask.GetMask ("Floor");    
+    }
 
-    Move (h, v);
-	}
+    void Start () {
+        this.playerRigidbody = GetComponent<Rigidbody> ();
+    }
 
-  void Move (float h, float v)
-  {
-    // TODO: Support rotation of the camera
-    // Set the movement vector based on the axis input.
-    movement.Set (h, 0f, v);
+    void FixedUpdate () {
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
 
-    // Normalise the movement vector and make it proportional to the speed per second.
-    movement = movement.normalized * playerSpeed * Time.deltaTime;
+        Move (h, v);
+        Turn ();
+    }
 
-    // Move the player to it's current position plus the movement.
-    playerRigidbody.MovePosition (transform.position + movement);
-  }
+    void Move (float h, float v) {
+        // Set the movement vector based on the axis input.
+        this.movement.Set (h, 0f, v);
+
+        // Normalize the movement vector and make it proportional to the speed per second.
+        this.movement = this.movement.normalized * this.playerSpeed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        this.playerRigidbody.MovePosition (this.transform.position + this.movement);
+    }
+
+    void Turn () {
+        Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        RaycastHit cameraRayFloorHit;
+        if (Physics.Raycast (cameraRay, out cameraRayFloorHit, 100f, floorMask)) {
+            Vector3 playerToFloorHit = cameraRayFloorHit.point - this.transform.position;
+            playerToFloorHit.y = 0;
+
+            Quaternion playerRotation = Quaternion.LookRotation (playerToFloorHit);
+            this.playerRigidbody.MoveRotation (playerRotation);
+        }
+    }
 }
