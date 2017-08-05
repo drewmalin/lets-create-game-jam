80a92db2e0d1b38f4ba0be4628a3fec742d1042e
Drew Malin <drew.malin@gmail.com>
2017-08-05 10:09:59 -0700

    New CameraFollow script which pins the camera to the movement of the main character

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Main camera movement behavior.
+ * 
+ * This class controls the automatic movement of the main camera, pinning its movement to the position of
+ * a provided Transform. Slight movement lag may be introduced by adjusting the movement smoothing field.
+ */
+public class CameraFollow : MonoBehaviour {
+
+    public Transform cameraTarget;
+    public float movementSmoothing = 5f;
+
+    Vector3 vectorToTarget;
+	
+    void Start() {
+        vectorToTarget = this.transform.position - this.cameraTarget.position;
+    }
+
+	void Update () {
+        Vector3 targetCameraPosition = this.cameraTarget.position + this.vectorToTarget;
+        transform.position = Vector3.Lerp (transform.position, targetCameraPosition, movementSmoothing * Time.deltaTime);
+	}
+
+    /*
+     * Returns a Vector3 representing the movement direction from the camera's current position to the
+     * camera's target.
+     */
+    private Vector3 GetVectorToCameraTarget() {
+        return this.transform.position - this.cameraTarget.position;
+    }
+}
