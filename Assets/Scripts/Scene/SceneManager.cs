72070518828d2db58db98935a4a870d6c85f0eac
Drew Malin <drew.malin@gmail.com>
2017-08-08 20:14:20 -0700

    Slight beginnings of the mall. New SceneManager, Enemy, EnemyController, SpawnPoint. Shuffling around directories

---
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * The scene manager is responsible for manging the enemy spawn points.
+ */
+public class SceneManager : MonoBehaviour {
+
+    [SerializeField]
+    private List<SpawnPoint> enemySpawnPoints;
+
+	void Start () {
+        SpawnEnemies ();
+	}
+
+    private void SpawnEnemies() {
+        foreach (SpawnPoint spawnPoint in this.enemySpawnPoints) {
+            StartCoroutine (SpawnEnemies (spawnPoint));
+        }
+    }
+
+    private IEnumerator SpawnEnemies(SpawnPoint spawnPoint) {
+        for (int i = spawnPoint.GetCountOfObjectsToSpawn (); i > 0; i--) {
+            Instantiate (spawnPoint.GetObjectToSpawn (), spawnPoint.GetSpawnLocation ().position, spawnPoint.GetSpawnLocation ().rotation);
+            yield return new WaitForSeconds (spawnPoint.GetSpawnInterval ());
+        }
+    }
+}
